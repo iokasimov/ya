ad11458476ce2f04f25871c432f26d7da580438f
Murat <murat@iMac.local>
2023-09-17 20:32:54 +0700

    Change parameters order in Transformation type class

--- Ya/Algebra/Definition.hs
+++ Ya/Algebra/Definition.hs
@@ -5,34 +5,34 @@ module Ya.Algebra.Definition where
 
 import Ya.Algebra.Abstract
 
-class Transformation v f g from into where
+class Transformation v from into f g where
 	transformation :: v from s t -> into (f s) (g t)
 
 transform :: forall v from into f g s t .
-	Transformation v f g from into =>
+	Transformation v from into f g =>
 	Castable Dual Arrow (v from s t) =>
 	Supertype (v from s t) -> into (f s) (g t)
-transform from = transformation @v @f @g @from @into @s @t (wrap @Arrow from)
+transform from = transformation @v @from @into @f @g @s @t (wrap @Arrow from)
 
 component :: forall v from into f g t .
 	Category from =>
 	(Supertype (v from t t) ~ from t t) =>
 	Castable Dual Arrow (v from t t) =>
-	Transformation v f g from into =>
+	Transformation v from into f g =>
 	into (f t) (g t)
-component = transformation @v @f @g @from @into @_ @t (wrap @Arrow identity)
+component = transformation @v @from @into @f @g @_ @t (wrap @Arrow identity)
 
 {- [LAW] Associativity: compose f (compose g) ≡ compose (compose f g) -}
 class
-	( forall i . Transformation Flat (U_I_II from i) (U_I_II from i) from Arrow
-	, forall i . Transformation Dual (U_II_I from i) (U_II_I from i) from Arrow
+	( forall i . Transformation Flat from Arrow (U_I_II from i) (U_I_II from i)
+	, forall i . Transformation Dual from Arrow (U_II_I from i) (U_II_I from i)
 	) => Semigroupoid from where
 	compose :: from s t -> from i s -> from i t
 	compose post pre = let U_I_II y = transform @Flat post (U_I_II pre) in y
 
 deriving instance
-	( forall i . Transformation Flat (U_I_II from i) (U_I_II from i) from Arrow
-	, forall i . Transformation Dual (U_II_I from i) (U_II_I from i) from Arrow
+	( forall i . Transformation Flat from Arrow (U_I_II from i) (U_I_II from i)
+	, forall i . Transformation Dual from Arrow (U_II_I from i) (U_II_I from i)
 	) => Semigroupoid from
 
 {- [LAW] Left identity: identity . f ≡ f -}
@@ -40,9 +40,9 @@ deriving instance
 class Semigroupoid from => Category from
 	where identity :: from s s
 
-class (m from, mm into, Transformation v f f from into)
+class (m from, mm into, Transformation v from into f f)
 	=> Mapping m mm v from into f
-deriving instance (m from, mm into, Transformation v f f from into)
+deriving instance (m from, mm into, Transformation v from into f f)
 	=> Mapping m mm v from into f
 
 {- [LAW] Composition preserving: transformation (f . g) ≡ transformation f . transformation g -}
@@ -70,10 +70,10 @@ type Semi v functor = Mapping Semigroupoid Semigroupoid v
 -- Doesn't work with Semi Functor declarations 
 type Endo v functor into = functor v into into
 
-class (t v f g from into, f' v from into f, g' v from into g)
-	=> Compositional f' g' t v f g from into
-deriving instance (t v f g from into, f' v from into f, g' v from into g) 
-	=> Compositional f' g' t v f g from into
+class (t v from into f g, f' v from into f, g' v from into g)
+	=> Compositional f' g' t v from into f g
+deriving instance (t v from into f g, f' v from into f, g' v from into g) 
+	=> Compositional f' g' t v from into f g
 
 {- LAW: transformation @g @g morphism . component @f @g = component @f @g . transformation morphism @f @f -}
 type Natural = Compositional Functor Functor
@@ -84,10 +84,10 @@ type Contravariant f = f Dual
 
 type Kleisli = U_I_T_II
 
-instance Transformation Flat (U_I_II Arrow s) (U_I_II Arrow s) Arrow Arrow
+instance Transformation Flat Arrow Arrow (U_I_II Arrow s) (U_I_II Arrow s)
 	where transformation (U_I_II from) (U_I_II between) = U_I_II (\x -> from (between x))
 
-instance Transformation Dual (U_II_I Arrow t) (U_II_I Arrow t) Arrow Arrow
+instance Transformation Dual Arrow Arrow (U_II_I Arrow t) (U_II_I Arrow t)
 	where transformation (U_II_I from) (U_II_I between) = U_II_I (\x -> between (from x))
 
 instance Category Arrow where
@@ -103,14 +103,14 @@ type family Representation t where
 	Representation (U_I_I (/\)) = Unit \/ Unit
 
 class
-	 ( Compositional x x Transformation v t (v hom (Representation t)) from into
-	 , Compositional x x Transformation v (v hom (Representation t)) t from into
+	 ( Compositional x x Transformation v from into t (v hom (Representation t))
+	 , Compositional x x Transformation v from into (v hom (Representation t)) t
 	 ) => Representable hom v x from into t
 
 -- TODO: define these instances in Algebra module
 deriving instance
-	( Compositional x x Transformation v t (v hom (Representation t)) from into
-	, Compositional x x Transformation v (v hom (Representation t)) t from into
+	( Compositional x x Transformation v from into t (v hom (Representation t))
+	, Compositional x x Transformation v from into (v hom (Representation t)) t
 	) => Representable hom v x from into t
 
 type family Neutral p where
@@ -120,8 +120,8 @@ type family Neutral p where
 type Day = U_UU_UUU_UUUU_T_TT_I_II_III (/\)
 
 class
-	( Transformation v (Day (v from) p pp f f i ii) f from into
-	, Transformation v (Day (v from) p pp I f i ii) f from into
-	, Transformation v (Day (v from) p pp f I i ii) f from into
-	, Transformation v (v from (Neutral p)) f from into
+	( Transformation v from into (Day (v from) p pp f f i ii) f
+	, Transformation v from into (Day (v from) p pp I f i ii) f
+	, Transformation v from into (Day (v from) p pp f I i ii) f
+	, Transformation v from into (v from (Neutral p)) f
 	) => Monoidal v f p pp from into i ii  
