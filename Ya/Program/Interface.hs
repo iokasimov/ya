0e0f5f6c3486746babec8025c4bef0062adf5ec6
Murat Kasimov <iokasimov.m@gmail.com>
2024-01-28 16:01:16 +0700

    Generalize `Scrollable` type class

--- Ya/Program/Interface.hs
+++ Ya/Program/Interface.hs
@@ -73,7 +73,7 @@ instance Stack (Construction Optional) Statefully where
 		Nonempty @List (Yet x None) -> These `i_i` Nonempty @List `i` Yet x None `i_i` None
 	push x = W_I_I_II `a` U_I_UU_II_III `yi` \s -> These `i` rewrap (Next x) s `i` x
 
-type family Scrolling datastructure where
+type family Scrolling datastructure = result | result -> datastructure where
 	Scrolling List = U_T_I_TT_I LM Identity (U_T_I_TT_I LM (Backward List) (Forward List))
 
 instance Mapping Straight Straight Arrow Arrow
@@ -89,14 +89,14 @@ instance Mapping Straight Straight Arrow Arrow
 type family Orientation datastructure where
 	Orientation List = Horizontal
 
-class Scrollable morphism datastructure where
+class Scrollable datastructure morphism where
 	scroll :: Orientation datastructure -> morphism
 		`TI` Scrolling datastructure item
 		`TI` Optional (Scrolling datastructure item)
 
 -- TODO: try use the fact that `Horizontal` ~ `Boolean`
 -- `Boolean` is `Representative` for `U_I_I LM`
-instance Scrollable Statefully List where
+instance Scrollable List Statefully where
 	scroll (That _) = W_I_I_II `a` U_I_UU_II_III `yi` \case
 		previous@(U_T_I_TT_I (These (Identity x) (U_T_I_TT_I (These (T_'_I (T_TT_I bs)) (T_'_I (List (Yet f fs))))))) -> These
 			(U_T_I_TT_I (These (Identity f) (U_T_I_TT_I (These (T_'_I (List (Yet x (bs `yo` unwrap)))) (T_'_I (T_TT_I / fs `yo` wrap ))))))
@@ -108,11 +108,11 @@ instance Scrollable Statefully List where
 			(Some previous)
 		previous@(_) -> These previous None
 
-instance {-# OVERLAPS #-} Scrollable Statefully t => Scrollable Transition t where
+instance {-# OVERLAPS #-} Scrollable t Statefully => Scrollable t Transition where
 	scroll orient = W_I_II_I `compose` U_I_UU_II_III
 		`compose` fio (\(These x y) -> These y x)
 		`compose` unwrap `compose` unwrap
-		/ scroll @Statefully @t orient
+		/ scroll @t @Statefully orient
 
 -- TODO: experimental and highly likely very inefficient
 -- TODO: should we defined with a wrapper since it's not the only possible implementation?
