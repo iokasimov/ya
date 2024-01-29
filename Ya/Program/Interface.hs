835c153c0f9bab252b7c5468c71eb22bec31ab69
Murat Kasimov <iokasimov.m@gmail.com>
2024-01-29 15:27:55 +0700

    Change type arguments order in `scroll` method

--- Ya/Program/Interface.hs
+++ Ya/Program/Interface.hs
@@ -89,14 +89,14 @@ instance Mapping Straight Straight Arrow Arrow
 type family Orientation datastructure where
 	Orientation List = Horizontal
 
-class Scrollable datastructure morphism where
+class Scrollable datastructure item morphism where
 	scroll :: Orientation datastructure -> morphism
 		`TI` Scrolling datastructure item
 		`TI` Optional (Scrolling datastructure item)
 
 -- TODO: try use the fact that `Horizontal` ~ `Boolean`
 -- `Boolean` is `Representative` for `U_I_I LM`
-instance Scrollable List Statefully where
+instance Scrollable List item Statefully where
 	scroll (That _) = W_I_I_II `a` U_I_UU_II_III `yi` \case
 		previous@(U_T_I_TT_I (These (Identity x) (U_T_I_TT_I (These (T_'_I (T_TT_I bs)) (T_'_I (List (Yet f fs))))))) -> These
 			(U_T_I_TT_I (These (Identity f) (U_T_I_TT_I (These (T_'_I (List (Yet x (bs `yo` unwrap)))) (T_'_I (T_TT_I / fs `yo` wrap ))))))
@@ -108,11 +108,11 @@ instance Scrollable List Statefully where
 			(Some previous)
 		previous@(_) -> These previous None
 
-instance {-# OVERLAPS #-} Scrollable t Statefully => Scrollable t Transition where
+instance {-# OVERLAPS #-} Scrollable t item Statefully => Scrollable t item Transition where
 	scroll orient = W_I_II_I `compose` U_I_UU_II_III
 		`compose` fio (\(These x y) -> These y x)
 		`compose` unwrap `compose` unwrap
-		/ scroll @t @Statefully orient
+		/ scroll @t @item @Statefully orient
 
 -- TODO: experimental and highly likely very inefficient
 -- TODO: should we defined with a wrapper since it's not the only possible implementation?
