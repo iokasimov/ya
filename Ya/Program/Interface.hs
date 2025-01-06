aa2ab553e77e8d671b4effe933d4a4407664de3e
Murat Kasimov <iokasimov.m@gmail.com>
2025-01-06 14:10:15 +0700

    Prototype `Sliding List`

--- Ya/Program/Interface.hs
+++ Ya/Program/Interface.hs
@@ -239,6 +239,84 @@ instance Scrollable (Construction (Optional `T'TT'I` Construction Optional)) ite
     `hv__` Only (Tree `hv` unwrap focus `hv__` to @(Nonempty List) `hv` scrolling_list_tree `yo` unwrap @AR `yi` unwrap @AR)
      `lu` unwrap shafted)
 
+type family Sliding datastructure = result | result -> datastructure where
+ -- Ideally there should be `Queue` instead of `List`, but we don't have it for now
+ Sliding (Optional `T'TT'I` Construction Optional) = List `LM'T'I'TT'I` Shafted List
+
+class Scrollable datastructure item
+ => Slidable datastructure item where
+ slide :: Scroller datastructure `AR_` Automation `WR` Sliding datastructure item `WR` Scrolled datastructure item `WR` Sliding datastructure item
+ extend :: Scroller datastructure `AR_` Automation `WR` Sliding datastructure item `WR` Scrolled datastructure item `WR` Sliding datastructure item
+
+instance Slidable (Optional `T'TT'I` Construction Optional) item where
+ slide way x = is
+  `li` is `hu` (None () `lu` x)
+  `la` is `ho'he` foi @_ @Arrow Some
+  `li` slide_future `he'he'hv` x where
+
+  slide_future = enter @(State `WR` Sliding List item `JNT` Halts)
+   `yuk____` New `ha` State `hv___` Event `hv` pop
+   `ha__'he` Scope `hv` at @(List item)
+   `yok____` Try `ha` Maybe
+   `yok____` New `ha` State `ha__` Event `ha` push
+   `ho_'ha'he` Scope @(Shafted List item) at
+      `ho'he` Scope @(Reverse List item) at
+      `ho'he` Scope self
+   `yuk____` New `ha` State `hv____` Event `hv` pop
+   `ha___'he` Scope `hv` at @(Shafted List item)
+      `ho'he` Scope @(Forward List item) at
+      `ho'he` Scope self
+   `yok____` Try `ha` Maybe
+   `yok____` New `ha` State `ha____` Event `ha` window_future
+   `ho_'ha'he` Scope `hv` at @(List item)
+
+  slide_passed = enter @(State `WR` Sliding List item `JNT` Halts)
+   `yuk____` New `ha` State `hv____` Event `hv` pop
+   `ha___'he` Scope @(Shafted List item) at
+      `ho'he` Scope @(Reverse List item) at
+      `ho'he` Scope self
+   `yok____` Try `ha` Maybe
+   `yok____` New `ha` State `ha__` Event `ha` window_extract_last
+   `ho_'ha'he` Scope `hv` at @(List item)
+   `yok____` Try `ha` Maybe
+   `yok____` New `ha` State `ha__` Event `ha` push
+   `ho_'ha'he` Scope @(Shafted List item) at
+      `ho'he` Scope @(Forward List item) at
+      `ho'he` Scope self
+
+  window_future r w = (w `yokl` Prior `ha` New `ha` State `ha` Event `ha` push `he'he'hv___` List `ha` Item r `ha` Maybe `hv` Last) `yui` r
+
+  window_extract_last passed window =
+   push @List passed window `yi` that
+    `yokl` Forth `ha` New `ha` State `ha` Event `ha` push
+    `he'he'hv___` it `hv` Empty @List
+    `yi__` that `ho` pop @List
+
+ extend way x = is
+  `li` is `hu` (None () `lu` x)
+  `la` is `ho'he` foi @_ @Arrow Some
+  `li` (extend_passed `lv` extend_future `li` way) `he'he'hv` x where
+
+  extend_passed = enter @(State `WR` Sliding List item `JNT` Halts)
+   `yuk____` New `ha` State `hv____` Event `hv` pop @List
+   `ha___'he` Scope @(Shafted List item) at
+      `ho'he` Scope @(Reverse List item) at
+      `ho'he` Scope self
+   `yok____` Try `ha` Maybe
+   `yok____` New `ha` State `ha__` Event `ha` push
+   `ho_'ha'he` Scope `hv` at @(List item)
+
+  extend_future = enter @(State `WR` Sliding List item `JNT` Halts)
+   `yuk____` New `ha` State `hv____` Event `hv` pop
+   `ha___'he` Scope `hv` at @(Shafted List item)
+      `ho'he` Scope @(Forward List item) at
+      `ho'he` Scope self
+   `yok____` Try `ha` Maybe
+   `yok____` New `ha` State `ha____` Event `ha` window_future
+   `ho_'ha'he` Scope `hv` at @(List item)
+
+  window_future r w = (w `yokl` Prior `ha` New `ha` State `ha` Event `ha` push `he'he'hv___` List `ha` Item r `ha` Maybe `hv` Last) `yui` r
+
 -- TODO: instance Scrollable (Construction (U_I_I LM `T'TT'I` Optional)) item where
 
 -- TODO: think about alternative implementations
