fea3bf56aaf01ae6f837d23f4933249857fb655e
Murat Kasimov <iokasimov.m@gmail.com>
2025-05-02 16:34:03 +0700

    Remove 5/6/7 length composed operators

--- Ya/Operators/Generated.hs
+++ Ya/Operators/Generated.hs
@@ -4,6 +4,3 @@ module Ya.Operators.Generated (module Exports) where
 import Ya.Operators.Generated.XX as Exports
 import Ya.Operators.Generated.XXX as Exports
 import Ya.Operators.Generated.XXXX as Exports
-import Ya.Operators.Generated.XXXXX as Exports
-import Ya.Operators.Generated.XXXXXX as Exports
-import Ya.Operators.Generated.XXXXXXX as Exports
