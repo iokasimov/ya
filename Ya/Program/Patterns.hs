2dc91e7fd188c8845233b712a5bcdccf7fb4bf2b
Murat Kasimov <iokasimov.m@gmail.com>
2025-07-20 09:08:18 +0700

    Remove `Appointed` pattern synonymous

--- Ya/Program/Patterns.hs
+++ Ya/Program/Patterns.hs
@@ -144,11 +144,6 @@ type Match = U_I_UU_M_I_II_II (AR) (S)
 pattern Match :: forall ee e . (e `AR_` (S) (M e ee) ee) -> Match e ee
 pattern Match x = U_I_UU_M_I_II_II @(AR) @(S) x
 
-type Appointed = T'II'TT'I'III (AR) (S) Unit
-
-pattern Appointed :: forall ee e . (e `AR_` Unit `S` ee) `AR___` Appointed e ee
-pattern Appointed x = T'II'TT'I'III @(AR) @(S) x
-
 type Automation e ee eee = e `AR_` ee `P` eee
 
 type Transition = T'I'TT'II'I (AR) (P)
