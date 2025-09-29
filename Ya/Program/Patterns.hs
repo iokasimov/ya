31fd5e6a888e593810c501f8aff34796dadf8fab
Murat Kasimov <iokasimov.m@gmail.com>
2025-09-29 23:19:50 +0700

    Remove `Close` wrapper

--- Ya/Program/Patterns.hs
+++ Ya/Program/Patterns.hs
@@ -108,15 +108,9 @@ pattern Reach x = T'I'II (This x)
 pattern Wrong :: i -> Stops i ii
 pattern Wrong x = T'I'II (This x)
 
--- TODO: remove
-pattern Close :: i -> Stops i ii
-pattern Close x = T'I'II (This x)
-
-pattern Valid, Going :: ii -> Stops i ii
+pattern Valid, Going, Ok :: ii -> Stops i ii
 pattern Valid x = T'I'II (That x)
 pattern Going x = T'I'II (That x)
-
-pattern Ok :: ii -> Stops i ii
 pattern Ok x = T'I'II (That x)
 
 type Probably = T'I'II (S)
