dc29bd334f4e63d0a3ac750fdfbb3981b87501ec
Murat Kasimov <iokasimov.m@gmail.com>
2025-10-02 22:48:13 +0700

    Define special `Spare` transformation

--- Ya/Program/Interface.hs
+++ Ya/Program/Interface.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE UndecidableInstances #-}
-module Ya.Program.Interface (module Exports, shaft, pattern Aloft, pattern Stump, pattern Merge) where
+module Ya.Program.Interface (module Exports, shaft, pattern Aloft, pattern Stump, pattern Spare, pattern Merge) where
 
 import Ya.Algebra
 import Ya.Operators
@@ -324,6 +324,14 @@ pattern Stump x = Label x
 instance Mapping T'I'II T'I'II (AR) (AR) (S'I'II e `L` S'I'II e `T` Void) (S'I'II Unit) where
  mapping = rewrap `identity` \from ->
   rewrap `identity` ((This `compose` constant Unit `la` That `compose` from) `compose` unwrap)
+
+pattern Spare :: forall i ii .
+ Component (AR) ((P'II'I i `T'TT'I` S'I'II Unit) `L` (P'II'I i `T'TT'I` S'I'II Unit) `T` Void) (S'I'II i) =>
+ P'II'I i (S'I'II Unit ii) `AR___` (P'II'I i `T'TT'I` S'I'II Unit) `L` (P'II'I i `T'TT'I` S'I'II Unit) `T` Void `T` ii
+pattern Spare x = Label (T'TT'I x)
+
+instance Mapping T'I'II T'I'II (AR) (AR) ((P'II'I i `T'TT'I` S'I'II Unit) `L` (P'II'I i `T'TT'I` S'I'II Unit) `T` Void) (S'I'II i) where
+ mapping = rewrap `identity` \from (Label (T'TT'I (T'II'I (These x i)))) -> Empty `hu` Error i `la` Valid `ha` from `li` x
 
 instance Semigroup (AR) Boolean where
  s (These x y) = x `lu'ys'la` Try y
